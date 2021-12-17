01d81c39b92128b8fdeac2d177bb1cc3409f45ae
Rohit Kumar Y <rohitkumar.y@tcs.com>
2021-12-17 20:48:32 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { CvComponent } from './cv/cv.component';
+import { EducationComponent } from './education/education.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+const routes: Routes = [
+{path:'',component:CvComponent,data: {animation: 'CvPage'} },
+{path:'education',component:EducationComponent,data: {animation: 'EducationPage'} },
+{path:'aboutme',component:AboutMeComponent,data: {animation: 'AboutMePage'} },
+{path:'organization',component:OrganizationComponent,data: {animation: 'OrganizationPage'} },
+{path:'technicalskills',component:TechnicalSkillsComponent,data: {animation: 'TechnicalSkillsPage'} },
+{path:'projects',component:ProjectsComponent,data: {animation: 'ProjectsPage'} },
+{path:'contact',component:ContactComponent,data: {animation: 'ContactPage'} }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RoutingDirective } from './routing.directive';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    OrganizationComponent,
+    TechnicalSkillsComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    NavbarComponent,
+    RoutingDirective,
+    CvComponent
   ],
   imports: [
     BrowserModule,
